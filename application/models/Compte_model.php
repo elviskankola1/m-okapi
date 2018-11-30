a3e13e2cb8f8ae60897ed64857a5a66e5bbbb197
elviskankola1 <elviskankola1@gmail.com>
2018-11-30 09:37:30 +0200

    ADDING COMMENT'S FOR MORE COMPREHENSION'S

--- application/models/Compte_model.php
+++ application/models/Compte_model.php
@@ -1,17 +1,28 @@
 <?php
 
 	if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|
+| ICI LE CONSTRUCTEUR DE LA CLASSE 
 
-	class unique_model extends CI_Model {
+
+*/
+	class Compte_model extends CI_Model {
 
         private $table_connexion = 'login';
         function __construct(){
             parent::__construct();
         }
 
+        public function Create_Account_User(){
+
+
+        }
         public function Sign_In_User($login ='', $pass =''){
 
             $query = $this->db->where(['login'=>$login,'pass'=>$pass])->get($this->table_connexion);
             return $query;
         }
+
+       
     }
