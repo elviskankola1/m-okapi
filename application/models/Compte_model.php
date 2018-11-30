b9aaf493c1d9b81ffe88b9e8f285ed8ec53e76a1
elviskankola1 <elviskankola1@gmail.com>
2018-11-30 09:43:49 +0200

    COMMENT

--- application/models/Compte_model.php
+++ application/models/Compte_model.php
@@ -15,14 +15,19 @@
         function __construct(){
             parent::__construct();
         }
-
+    /*=============================================================================
+            ICI L' ACTION DE CREATION DU COMPTE 
+     ===============================================================================*/
         public function Create_Account_User(){
 
 
         }
+    /*=============================================================================
+            ICI L' ACTION DE SE LOGER
+     ===============================================================================*/
         public function Sign_In_User($login ='', $pass =''){
 
-            $query = $this->db->where(['login'=>$login,'pass'=>$pass])->get($this->table_connexion);
+            $query = $this->db->where(['login'=>$login,'pass'=>$pass])->get($this->table_connexion)->result();
             return $query;
         }
 
