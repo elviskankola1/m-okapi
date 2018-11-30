3f8dfba7ed293eb30a07b5848bc9a67e5d8a7c1a
elviskankola1 <elviskankola1@gmail.com>
2018-11-30 11:07:03 +0200

    NEW FUNCTION FOR LOG IN CONTROLLERS FOLDER / MOkapi.php

--- application/controllers/MOkapi.php
+++ application/controllers/MOkapi.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MOkapi extends CI_Controller{
 
-
+    function __construct(){
+        parent::__construct();
+    }
     public function index(){
         $this->load->view('mokapi_home');
     }
@@ -18,15 +20,28 @@ class MOkapi extends CI_Controller{
         $pass_user = sha1($this->input->post('pwd'));
         $confirm_pass_user = sha1($this->input->post('pwdc'));
         if ($pass_user == $confirm_pass_user && filter_var($email_user,FILTER_VALIDATE_EMAIL)) {
-            $verify_user_exist =  $this->compte_model->Sign_In_User($pseudo,$pass_user);
-            if (!$verify_user_exist) {
+            $verify_user_exist =  $this->compte_model->Sign_In_User($pseudo_user,$pass_user);
+            if ($verify_user_exist == false) {
 
-                $this->compte_model->Create_Account_User($name_user,$email_user, $pseudo_user, $password_user);
+                $this->compte_model->Create_Account_User($name_user,$email_user, $pseudo_user, $pass_user);
                 echo "VOUS AVEZ CREE UN COMPTE ET BIENVENU CHEZ NOUS!";
             }
-            
-           echo "LE PSEUDO EXISTE DEJA";
+        }else{
+            redirect('HTTP_FERERER');
         }
-        echo "CHECK YOUR EMAIL OR YOUR CONFIRM PASS";
+    
+    public function Login_In_Account(){
+
+        $pseudo_user = strip_tags($this->input->post('pseudo'));
+        $pass_user = sha1($this->input->post('pwd'));
+        $user =  $this->compte_model->Sign_In_User($pseudo_user,$pass_user);
+        if($user){
+            echo "vous etes bien sur votre compte!";
+        }else{
+            redirect('HTTP_FERERER');
+        }
+
+    }
+        
     }
 }
